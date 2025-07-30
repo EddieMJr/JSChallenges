335c91e4ab7b396ba6bd2cdab15a12c87ff5f05c
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-07-30 19:07:01 -0400

    Completed Medium Challenge & added to Easy

--- index.js
+++ index.js
@@ -2,17 +2,44 @@
 'use strict';
 function exercise(activity) {
     function exerciseDisplay() {
+        // Interpolated String
         return `Today's Exercise: ${activity}`;
     }
     return exerciseDisplay();
 }
+// Output: Today's exercise: Running
 console.log(exercise("Running"));
+// Output: Today's exercise: Swimming
+console.log(exercise("Swimming"));
+// Output: Today's exercise: Dancing
+console.log(exercise("Dancing"));
+// Output: Today's exercise: Fencing
+console.log(exercise("Fencing"));
 
 // Challenge 2: Medium
-
+'use strict';
+function sharePizza(slices, eaters) {
+    // Calculate the number of slices each person gets
+    let split = slices / eaters;
+    // Interpolated String
+    return `Each person gets ${split} slices of pizza from our ${slices} slice pizza.`;
+}
+// Output: Each person gets 4 slices of pizza from our 8 slice pizza
+console.log(sharePizza(8, 2));
+// Output: Each person gets 2.67 slices of pizza; from our 8 slice pizza
+console.log(sharePizza(8, 3));
+// Output: Each person gets 1.05 slices of pizza; from our 21 slice pizza
+console.log(sharePizza(21, 20));
+// Output: Each person gets 3.33 slices of pizza; from our 10 slice pizza
+console.log(sharePizza(10, 3));
 
 // Challenge 3: Hard
-
+/*'use strict';
+function () {
+    function() {
+        
+    }
+}*/
 
 // Challenge 4: Very Hard
 
